299aef4e005266f5bda26d46da91c5672822886e
Leslie DE JAGER <lde-jage@e4r4p5.wethinkcode.co.za>
2018-08-24 09:05:34 +0200

    libft

---
+++ 1stSemester/Algos/libft/ft_strdup.c
@@ -0,0 +1,34 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_strdup.c                                        :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: lde-jage <marvin@42.fr>                    +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2017/05/29 13:55:09 by lde-jage          #+#    #+#             */
+/*   Updated: 2017/06/01 15:12:00 by lde-jage         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "libft.h"
+
+char	*ft_strdup(const char *src)
+{
+	char	*dest;
+	int		i;
+
+	i = 0;
+	while (src[i] != '\0')
+		i++;
+	dest = (char *)malloc(i + 1);
+	if (dest == NULL)
+		return (NULL);
+	i = 0;
+	while (src[i] != '\0')
+	{
+		dest[i] = src[i];
+		i++;
+	}
+	dest[i] = '\0';
+	return (dest);
+}
